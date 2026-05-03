7100f7295a170ab0a729c4805ad2b8d3719930c9
Ishitha1 <230701117@rajalakshmi.edu.in>
2026-05-03 23:05:54 +0530

    absolute path in server.py in middleware

--- middleware/server.py
+++ middleware/server.py
@@ -25,10 +25,14 @@ import json
 import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = BASE_DIR / "frontend" / "dist"
+
+print("STATIC DIR:", STATIC_DIR)
+print("EXISTS:", STATIC_DIR.exists())
 
 app = Flask(
     __name__,
-    static_folder=str(BASE_DIR / "frontend" / "dist"),
+    static_folder=str(STATIC_DIR),
     static_url_path=""
 )
 
