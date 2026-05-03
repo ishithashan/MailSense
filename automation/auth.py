c16a50b90f792431ae2e079aaf34eddbbd52bf65
Ishitha1 <230701117@rajalakshmi.edu.in>
2026-05-03 19:35:06 +0530

    updated code

--- automation/auth.py
+++ automation/auth.py
@@ -1,4 +1,4 @@
-from googleapiclient.discovery import build
+"""from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import Flow
 from automation.config import SCOPES
 from pathlib import Path
@@ -32,6 +32,36 @@ def get_flow(state=None):
         scopes=SCOPES,
         state=state,
         redirect_uri=REDIRECT_URI   # 🔥 USE VARIABLE
+    )
+
+def build_gmail_service(credentials):
+    return build("gmail", "v1", credentials=credentials)
+
+def build_sheets_service(credentials):
+    return build("sheets", "v4", credentials=credentials)"""
+import os
+import json
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+from automation.config import SCOPES
+
+# Read credentials from ENV (NOT FILE)
+credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
+REDIRECT_URI = os.getenv(
+    "REDIRECT_URI",
+    "https://recmailsense.onrender.com/oauth2callback"
+)
+
+def get_flow(state=None):
+    if not credentials_json:
+        raise ValueError("GOOGLE_CREDENTIALS_JSON not set")
+
+    return Flow.from_client_config(
+        json.loads(credentials_json),
+        scopes=SCOPES,
+        state=state,
+        redirect_uri=REDIRECT_URI
     )
 
 def build_gmail_service(credentials):
