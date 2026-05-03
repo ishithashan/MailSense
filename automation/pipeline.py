de148235c9edb6c1613a204fd67a7b749e8014da
Ishitha1 <230701117@rajalakshmi.edu.in>
2026-05-04 03:01:11 +0530

    edited server.py

--- automation/pipeline.py
+++ automation/pipeline.py
@@ -1,13 +1,13 @@
 from automation.auth import build_gmail_service
 from automation.gmail_reader import fetch_emails_with_body
-from automation.email_writer import save_emails_to_excel
-
+from automation.excel_writer import save_emails_to_excel
+import json
 from google.oauth2.credentials import Credentials
 
 
 def run_pipeline(creds_json, user_email):
-    credentials = Credentials.from_authorized_user_info(creds_json)
-
+    #credentials = Credentials.from_authorized_user_info(creds_json)
+    credentials = Credentials.from_authorized_user_info(json.loads(creds_json))
     # Gmail service
     gmail_service = build_gmail_service(credentials)
 
--- run.py
+++ run.py
@@ -2,5 +2,10 @@ import os
 from middleware.server import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT
+
+    app.run(
+        host="0.0.0.0",   # IMPORTANT
+        port=port,
+        debug=False
+    )
